5ccd21d041ad21af043ecde5df3d11977b23e1cf
okmasa <66762335+okmasa@users.noreply.github.com>
2021-10-09 20:25:54 +0900

    ヘッダー部分のコンポーネント化

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    grobal: {
+      body: {
+        backgroudColor: "gray.100",
+        color: "gray.800"
+      }
+    }
+  }
+});
+export default theme;
